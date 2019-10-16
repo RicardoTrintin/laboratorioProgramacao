6f657d5f08ef0409d4f5bba6c856d5b1df63eaa7
Paulo Ricardo Rodrigues Trintin <20181CMQ.ADS0069@ACADEMICO.CAMAQUA>
2019-10-15 21:57:50 -0300

    Inicio

--- src/heranca/Automovel.java
+++ src/heranca/Automovel.java
@@ -1,14 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package heranca;
 
-/**
- *
- * @author 20181CMQ.ADS0069
- */
 public class Automovel {
+
+    String placa;
+    String cor;
+    String modelo;
+
+    public Automovel() {
+        placa = null;
+        cor = null;
+        modelo = null;
+    }
+    
+    
+    
     
 }
--- src/heranca/Tipo.java
+++ src/heranca/Tipo.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package heranca;
 
-/**
- *
- * @author 20181CMQ.ADS0069
- */
 public interface Tipo {
+
+    
     
 }
